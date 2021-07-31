4b66e6461daf7f6192b122d01016e70cf151904a
sushrut141 <sushrut.shivaswamy@gmail.com>
2021-07-31 09:29:12 +0530

    Add pagination to table bubble

--- src/views/table/index.js
+++ src/views/table/index.js
@@ -1,5 +1,5 @@
-import { isDefined } from "../../data/common";
-import { createElem, getMountPoint } from "../common";
+import { isDefined, isUndefined } from "../../data/common";
+import { createDropdown, createElem, getMountPoint } from "../common";
 import './table.css';
 
 /**
@@ -18,6 +18,7 @@ export function TableView(params) {
     this._page = 0;
     this._elem$ = undefined;
     this._headers$ = undefined;
+    this._paginator$ = undefined;
     this._render();
 }
 
@@ -28,6 +29,7 @@ TableView.prototype.update = function (params) {
     if (isDefined(params.viewConfig)) {
         this._viewConfig = viewConfig;
     }
+    this._page = 0;
     this._render();
 };
 
@@ -35,16 +37,19 @@ TableView.prototype._render = function () {
     const root$ = getMountPoint(this._viewConfig);
     if (isDefined(this._elem$)) {
         root$.removeChild(this._elem$);
+        root$.removeChild(this._paginator$);
     }
-    this._createTableHeaders();
-    this._createTableBody();
     const elem$ = createElem('table', {
         class: 'bubbles bubbles-table'
     });
+    this._createTableHeaders();
+    this._createTableBody();
+    this._createTablePaginator();
     elem$.appendChild(this._headers$);
     elem$.appendChild(this._body$);
     this._elem$ = elem$;
     root$.appendChild(this._elem$);
+    root$.appendChild(this._paginator$);
 };
 
 TableView.prototype._createTableHeaders = function () {
@@ -88,10 +93,54 @@ TableView.prototype._createTableBody = function () {
     this._body$ = body$;
 };
 
-TableView.prototype._createPaginator = function () {
+TableView.prototype._createTablePaginator = function () {
     const paginator$ = createElem('div', {
-        class: 'bubbles-table-paginator',
+        class: 'bubbles bubbles-table-paginator',
     });
+    // page size selector
+    const pageSizeSelector$ = createDropdown({
+        name: this._rowCount,
+        fields: [10, 25, 50],
+        value: this._rowCount,
+        onSelect: (value) => {
+            this._rowCount = parseInt(value, 10);
+            this._page = 0;
+            this._render();
+        },
+    });
+    // row range indicator
+    pageSizeSelector$.classList.add('bubbles-table-paginator__pagesize');
+    const start = (this._page * this._rowCount) + 1;
+    const end = start + this._rowCount - 1;
+    const paginationRange$ = createElem('div', {
+        class: 'bubbles-table-paginator__range'
+    }, `Showing rows ${start}-${end} of ${this._data.length}`);
+    paginator$.appendChild(paginationRange$);
+    paginator$.appendChild(pageSizeSelector$);
+    this._createPaginationArrows(paginator$);
+    this._paginator$ = paginator$;
+};
 
+TableView.prototype._createPaginationArrows = function (mount$) {
+    const leftArrow$ = createElem('button', {
+        class: 'bubbles-table-paginator__arrow',
+    }, '<');
+    const rightArrow$ = createElem('button', {
+        class: 'bubbles-table-paginator__arrow',
+    }, '>');
+    leftArrow$.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        this._page -= 1;
+        this._render();
+    });
+    rightArrow$.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        this._page += 1;
+        this._render();
+    });
+    leftArrow$.disabled = this._page == 0;
+    rightArrow$.disabled = this._page === Math.ceil(this._data.length / this._rowCount);
+    mount$.appendChild(leftArrow$);
+    mount$.appendChild(rightArrow$);
 };
 
